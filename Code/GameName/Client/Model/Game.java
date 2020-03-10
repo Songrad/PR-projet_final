45986e95a6a6f3d443a9522227ef78ec40f86631
Deperrois Dylan <43884685+dylandprs@users.noreply.github.com>
2020-03-10 11:08:19 +0100

    Rename class

--- Code/GameName/Client/Model/Game.java
+++ Code/GameName/Client/Model/Game.java
@@ -1,11 +1,19 @@
+/*
+ * 
+ */
+
 import java.util.ArrayList;
-public class Game {
+
+public class Game
+{
 	private ArrayList<Player> listPlayer;
 	private ArrayList<GameObject> listObject;
-	public Board(){
+
+	public Board() {
 		listPlayer = new ArrayList<Player>();
 		listObject = new ArrayList<GameObject>();
 		listPlayer.add(new Player(10, 20, "TOTO"));
+
 		for (int i = 0;i < 10 ; i++ ) {
 			int x = (int)(Math.random()*300);
 			int y = (int)(Math.random()*300);
