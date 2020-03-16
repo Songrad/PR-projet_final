75f6dc38311867279ff28dd29ec17a3e4f1f593c
unknown <deperrois.dylan@gmail.com>
2020-03-16 11:29:00 +0100

    Comment

--- Code/GottaGoFat/Client/src/Controler/GameStart.java
+++ Code/GottaGoFat/Client/src/Controler/GameStart.java
@@ -3,22 +3,48 @@ package Controler;
 import Model.*;
 import View.*;
 
+/**
+ * Classe servant de controleur pour le lancement du jeu et du visuel
+ * @author Jérémy AUZOU, Dylan DÉPERROIS, Patrice MAISONNEUVE, Edouard VARIN
+ */
+
 public class GameStart {
+
+	/**
+	 * Variable
+	 */
 	private Game model;
-	public GameStart(){
+
+	/**
+	 * Contructeur par défaut
+	 */
+	public GameStart() {
 		model = new Game((int)(500+Math.random()*501));
 		FrameGame view = new FrameGame(model);
 	}
 
-	public static void main(String[] args) {
-		new GameStart();
-	}
-
-	public void setCoordPlayer(int x,int y, int idP){
+	/**
+	 * Méthode demandant le changement de coordonnées au métier
+	 * @param int x
+	 * @param int y
+	 * @param int idP
+	 */
+	public void setCoordPlayer(int x,int y, int idP) {
 		model.setCoordPlayer(x, y, idP);
 	}
 
-	public boolean toucheItem(int idP){
+	/**
+	 * Méthode demandant au métier si le joueur à touché l'objet
+	 * @param int idP
+	 */
+	public boolean toucheItem(int idP) {
 		return model.toucheItem(idP);
+	}
+
+	/**
+	 * Main de lancement du jeu
+	 */
+	public static void main(String[] args) {
+		new GameStart();
 	}
 }
